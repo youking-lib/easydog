eafd91d246e82c9463cdd00515adfb8d38ec257f
kingyong <whistleyong@tencent.com>
2019-05-31 23:01:37 +0800

    fixed logger plugin

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ export const loggerMiddleWare = (opt = {}) => store => dispatch => {
   return ({ module, actionName, setState }, ...args) => {
     opt.beforeDispath && opt.beforeDispath(actionName, module)
 
-    dispatch({ module, actionName, setState: (...args) => {
+    return dispatch({ module, actionName, setState: (...args) => {
       opt.beforeSetState && opt.beforeSetState(actionName, module)
       setState(...args)
       opt.afterSetState && opt.afterSetState(actionName, module)
